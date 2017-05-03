28f78401278c3865d9941fef3ad38b11bc96f7e0
msalmanzafar <m.salman.zafar@gmail.com>
2017-05-03 17:16:01 +0500

    My First Native App

---
+++ AwesomeProject/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text,View} from 'react-native';
+
+class NavBar extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Welcome to my App</Text>
+            </View>
+        )
+    }
+}
+
+export default NavBar;
