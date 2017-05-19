360ea9add9a3e29b4b3cb653a798b1ffd6fed272
msalmanzafar <m.salman.zafar292@gmail.com>
2017-05-19 02:25:40 -0700

    Authentication complete

--- Authentication With Firebase/src/index.js
+++ Authentication With Firebase/src/index.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header,Button } from './Components/Common';
+import { Header, CardSection, Button, Spinner } from './Components/Common';
 import LoginForm from './Components/loginForm'
+
+
 
 class SignUp extends Component {
     state = {
         logedIn: null,
-    }
+    };
     componentWillMount() {
-        firebase.initializeApp(
-            {
-                apiKey: "AIzaSyArVpiyJ0wPxATriQBnPzvbCqjIBVWSppc",
-                authDomain: "sky-auth.firebaseapp.com",
-                databaseURL: "https://sky-auth.firebaseio.com",
-                projectId: "sky-auth",
-                storageBucket: "sky-auth.appspot.com",
-                messagingSenderId: "936918632809"
-            }
-        );
+        firebase.initializeApp( {
+            apiKey: "AIzaSyArVpiyJ0wPxATriQBnPzvbCqjIBVWSppc",
+            authDomain: "sky-auth.firebaseapp.com",
+            databaseURL: "https://sky-auth.firebaseio.com",
+            projectId: "sky-auth",
+            storageBucket: "sky-auth.appspot.com",
+            messagingSenderId: "936918632809"
+        });
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 this.setState({ logedIn: true });
@@ -30,17 +30,28 @@ class SignUp extends Component {
     }
 
     AuthRenderContent() {
-        if (this.state.logedIn) {
-            return (
-                <Button>Log Out</Button>                
-            );
-        } else {
-            return(
-                <LoginForm />
-            ) ;
+        switch (this.state.logedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+                    </CardSection>
+                );
+            case false:
+                return (
+                    <LoginForm />
+                );
+            default:
+                return (
+                    <CardSection>
+                        <Spinner size='large' />
+                    </CardSection>
+                );
         }
+
     }
     render() {
+        console.log(this.state.logedIn, "user info")
         return (
             <View>
                 <Header headerText="Inventory Store" />
