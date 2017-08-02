bec1efe158e5257b3f5d193a59845cf422f276da
msalmanzafar <m.salman.zafar292@gmail.com>
2017-08-02 15:59:18 +0500

    Options

--- Demo/PatientTracker/src/Component/home/options.js
+++ Demo/PatientTracker/src/Component/home/options.js
@@ -7,16 +7,31 @@ class Options extends Component {
     constructor(props) {
         super(props);
         this.ButtonLogIn = this.ButtonLogIn.bind(this);
-        // this.DoctorPanel = this.DoctorPanel.bind(this);
+        this.doctor = this.doctor.bind(this);
+        this.patient = this.patient.bind(this);
+        this.about = this.about.bind(this);
+        this.exit = this.exit.bind(this);
 
     }
     ButtonLogIn() {
         console.log("Login here");
     }
-    // Doctor() {
-    //     console.log("DoctorPanel here");
+    doctor() {
+        console.log("Doctor here");
 
-    // }
+    }
+    patient() {
+        console.log("Patient here");
+
+    }
+    about() {
+        console.log("About here");
+
+    }
+    exit() {
+        console.log("exit here");
+
+    }
     render() {
         return (
             <View>
@@ -27,22 +42,22 @@ class Options extends Component {
                         </Button>
                     </CardSection>
                     <CardSection>
-                        <Button >
+                        <Button onPress={this.doctor}>
                             <Text>Doctor</Text>
                         </Button>
                     </CardSection>
                     <CardSection>
-                        <Button >
+                        <Button onPress={this.patient}>
                             <Text>Patient</Text>
                         </Button>
                     </CardSection>
                     <CardSection>
-                        <Button >
+                        <Button onPress={this.about}>
                             <Text>About</Text>
                         </Button>
                     </CardSection>
                     <CardSection>
-                        <Button >
+                        <Button onPress={this.exit}>
                             <Text>Exit</Text>
                         </Button>
                     </CardSection>
